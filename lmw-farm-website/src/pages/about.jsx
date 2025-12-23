2e8dc1c45409b69d9da52020bfae94f70c28befd
srussett28 <167823549+srussett28@users.noreply.github.com>
2025-12-23 14:32:05 -0500

    Fix Navigation import case sensitivity

--- lmw-farm-website/src/pages/about.jsx
+++ lmw-farm-website/src/pages/about.jsx
@@ -1,5 +1,3 @@
-import LMWFamImage from '../assets/LMWFam.jpg';
-import sbrImage from '../assets/sbr.jpg';
 
 function About() {
   return (
@@ -72,7 +70,7 @@ function About() {
                 {/* Right Column - Family Photo */}
                 <div className="md:w-1/3 flex items-start justify-center">
                   <img 
-                    src={LMWFamImage}
+                    src="/src/assets/LMWFam.jpg" 
                     alt="The LMW Farm Family" 
                     className="rounded-lg shadow-2xl w-full h-auto object-cover"
                   />
@@ -214,7 +212,7 @@ function About() {
                 {/* Right Column - Founder Photo */}
                 <div className="md:w-1/3 flex items-start justify-center">
                   <img 
-                    src={sbrImage}
+                    src="/src/assets/sbr.jpg" 
                     alt="LMW Farm Founder" 
                     className="rounded-lg shadow-2xl w-full h-auto object-cover"
                   />
